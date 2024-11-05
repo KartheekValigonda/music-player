3b161d0761800febfde38dbf1919128cada6e488
Kartheek <valigondakartheek4@gmail.com>
2024-11-05 11:34:55 +0530

    added some more files

--- lib/core/home.dart
+++ lib/core/home.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'package:kartheek/common_widgets/coustom_text_styles.dart';
+import 'package:kartheek/controllers/player_controller.dart';
+import 'package:kartheek/core/color_pallet.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -11,8 +15,10 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
+    var controller = Get.put(PlayerController());
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.black,
         actions: [
           IconButton(onPressed: (){}, icon: const Icon(Icons.search, color: Colors.white))
         ],
@@ -25,9 +31,29 @@ class _HomeState extends State<Home> {
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: ListView.builder(
+          physics: const BouncingScrollPhysics(),
             itemCount:100,
             itemBuilder: (BuildContext context, int index){
-          return ;
+          return Container(
+            clipBehavior: Clip.antiAlias,
+            margin: EdgeInsets.only(bottom:4,),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+            ),
+              child: ListTile(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                title: Text("Music Name", style: ourStyle(
+                  family: bold,
+                  size: 15,),),
+                subtitle:Text("Artist Name", style: ourStyle(
+                  family: regular,
+                  size: 12,),),
+                leading: const Icon(Icons.music_note, color: Colors.white, size: 32,),
+                trailing: const Icon(Icons.play_arrow, color: Colors.white, size: 26,),
+              )
+          );
             }),
       )
     );
